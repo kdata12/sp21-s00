c76afd542cbdf00c94fb22c2d74fb352f2deaa8b
Kevin Vong <kevinvong0129@gmail.com>
2022-10-17 15:36:23 -0700

    lab3 finished

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -49,20 +49,27 @@ public class TestBuggyAList {
 
     @Test
     public void randomizedTest () {
-        AListNoResizing<Integer> L = new AListNoResizing<>();
+        AListNoResizing <Integer> expected = new AListNoResizing<>();
+        BuggyAList <Integer> Buggy = new BuggyAList<>();
 
-        int N = 500;
+        int N = 5000;
         for (int i = 0; i < N; i += 1) {
-            int operationNumber = StdRandom.uniform(0, 2);
+            int operationNumber = StdRandom.uniform(0, 3);
             if (operationNumber == 0) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);
-                L.addLast(randVal);
-                System.out.println("addLast(" + randVal + ")");
-            } else if (operationNumber == 1) {
-                // size
-                int size = L.size();
-                System.out.println("size: " + size);
+                expected.addLast(randVal);
+                Buggy.addLast(randVal);
+
+            } else if (operationNumber == 1 && expected.size() > 0) {
+                // get last
+                assertEquals(expected.getLast(), Buggy.getLast());
+
+
+            } else if (operationNumber == 2 && expected.size() > 0) {
+                // remove last
+                assertEquals(expected.removeLast(), Buggy.removeLast());
+
             }
         }
     }
